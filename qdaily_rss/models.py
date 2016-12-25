a3322e0222d9365861a4057f6c61dec662ca399b
Zizheng Wu <me@zizhengwu.com>
2016-12-25 21:18:39 +0800

    article model

--- qdaily_rss/models.py
+++ qdaily_rss/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.TextField()
+    url = models.URLField()
+    description = models.TextField()
+
